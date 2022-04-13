f89176395766b4e4ed9c6af93eafae02738cf3b9
YaChugunov <informed.looser@gmail.com>
2022-04-13 17:01:34 +0300

    3 files modified

--- src/TopBlock/TopBlockAdv.jsx
+++ src/TopBlock/TopBlockAdv.jsx
@@ -12,7 +12,14 @@ import React from 'react';
 export default function TopBlockAdv(props) {
   return (
     <div className="top-block-adv">
-      <h4>Реклама</h4>
+      <div>
+        <img src="https://avatars.mds.yandex.net/get-banana/28825/x25uPjdRZOI6MSKsMPUV7FTN_banana_20161021_120x90-blue-car-2x402x.png/orig" />
+      </div>
+      <h4>Рекламный заголовок</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+        malesuada urna, eget semper nulla.
+      </p>
     </div>
   );
 }
